66979cb6ad62d362f86801efc34a1d087026d53e
Houssam <houbou98@gmail.com>
2026-04-29 22:19:21 +0200

    add menu clause and cleanup debugging worked locally, lets test it deployed.

--- launcher.sh
+++ launcher.sh
@@ -8,8 +8,11 @@ export MESA_GLSL_VERSION_OVERRIDE=430
 CORE="$1"
 ROM="$2"
 
+# Menu mode - no ROM
+if [ "$CORE" = "menu" ]; then
+  dbus-launch retroarch --fullscreen 2>/tmp/ra.log
 # Check if it's an AppImage (Eden for Switch)
-if [[ "$CORE" == *.AppImage ]]; then
+elif [[ "$CORE" == *.AppImage ]]; then
   dbus-launch "$CORE" "$ROM" 2>/tmp/eden.log
 else
   # RetroArch for everything else
