b9ee29720b44c3013ee660dd2baf2036c47da5f6
Houssam <houbou98@gmail.com>
2026-05-25 07:27:27 +0200

    increase title font size

--- carousel.py
+++ carousel.py
@@ -68,7 +68,7 @@ class CarouselMenu(pyglet.window.Window):
         title = pyglet.text.Label(
             "HB SYSTEM",
             font_name="Press Start 2P",
-            font_size=100,
+            font_size=120,
             x=self.width // 2,
             y=self.height - 250,
             anchor_x='center',
